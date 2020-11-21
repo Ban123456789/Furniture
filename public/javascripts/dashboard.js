c381a5ab089b28114e184861d146b662cceb4b8f
CuteCorgi <55479773+Ban123456789@users.noreply.github.com>
2020-11-21 23:54:31 +0800

    body-parser 無法使用

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
+    // 提示
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
 
+    // 互動視窗
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').trigger('focus')
     })
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const { route } = require('.');
 var router = express.Router();
+var firebaseDb = require('../connection/firebase_admin');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,20 @@ router.get('/', function(req, res, next) {
 // todo 商品管理
 router.get('/addproducts', function(req, res, next) {
     res.render('dashboard/db-addproducts');
-  });
+    
+});
+router.post('/category/creat', function (res, req) {
+    console.log(req.body);
+});
+
+// todo 訂單查詢
+router.get('/orders', function(req, res, next) {
+    res.render('dashboard/db-orders');
+});
+
+// todo 其他
+router.get('/others', function(req, res, next) {
+    res.render('dashboard/db-others');
+});
 
 module.exports = router;

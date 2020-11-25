0ed51afba02a7a8a8469fbaa70093ab6e2c784a2
CuteCorgi <55479773+Ban123456789@users.noreply.github.com>
2020-11-25 23:53:03 +0800

    前端陳列商品+分類完成

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var firebaseDb = require('../connection/firebase_admin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,7 +14,31 @@ router.get('/main', function(req, res, next) {
 
 // todo 產品型錄
 router.get('/products', function(req, res, next) {
-  res.render('client/Products', { title: 'Expressssss' });
+  let categoriesArr = []; 
+  let productsArr = [];
+  let category = req.query.category || '全部商品';
+      // console.log(category);   
+      firebaseDb.ref('categories').once('value').then( data => {
+          data.forEach( categories => {
+              categoriesArr.push(categories.val());
+          });
+          // console.log(categoriesArr);
+          return firebaseDb.ref('products').once('value');
+      }).then( data => {
+          data.forEach( products => {
+              if(category === products.val().category){
+                  productsArr.push(products.val());
+              }else if(category === '全部商品'){
+                  productsArr.push(products.val());
+              }
+          });
+          // console.log(productsArr);
+          res.render('client/products', {
+              categoriesArr,
+              productsArr,
+              category
+          });
+      });
 });
 
 // todo 產品細節
